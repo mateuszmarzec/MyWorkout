0121bb73fda6bf5a1737618432fa48205135aea0
Mateusz Marzec <mateusz@macbook-pro-mateusz.home>
2021-11-19 21:06:47 +0100

    Adds python formatting

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -14,14 +14,14 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret_key")
 
 
 ALLOWED_HOSTS = ["api"]
